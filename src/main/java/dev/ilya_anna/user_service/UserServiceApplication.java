6f27fb47243cb8b318652516b0f1001063a1a44e
48-iq <ivn25ov@mail.ru>
2025-03-24 19:26:40 +0400

    initial commit

---
+++ src/main/java/dev/ilya_anna/user_service/UserServiceApplication.java
@@ -0,0 +1,14 @@
+package dev.ilya_anna.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
